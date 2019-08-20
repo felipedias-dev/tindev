0b975a7d9cd87c9d671203081012503e68c508ed
rafael-lip <felipedias.dev@hotmail.com>
2019-08-20 02:02:30 -0300

    Add Login page

---
+++ app/src/index.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+import Login from './pages/Login';
+
+export default function App() {
+  return (
+    <Login/>
+  );
+}
