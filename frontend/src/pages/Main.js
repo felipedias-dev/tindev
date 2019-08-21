d2e6a101dd627c699aefa025b2d433ca95da6568
rafael-lip <felipedias.dev@hotmail.com>
2019-08-21 03:21:30 -0300

    Main.js reorganized

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -10,22 +10,6 @@ import './Main.css';
 
 export default function Main({ match }) {
   const [users, setUsers] = useState([]);
-
-  async function handleLike(id) {
-    await api.post(`/devs/${id}/likes`, null, {
-      headers: { user: match.params.id }
-    });
-
-    setUsers(users.filter(user => user._id !== id));
-  }
-  
-  async function handleDislike(id) {
-    await api.post(`/devs/${id}/dislikes`, null, {
-      headers: { user: match.params.id }
-    });
-
-    setUsers(users.filter(user => user._id !== id));
-  }
   
   useEffect(() => {
     async function loadUsers() {
@@ -40,6 +24,22 @@ export default function Main({ match }) {
 
     loadUsers();
   }, [match.params.id]);
+  
+  async function handleLike(id) {
+    await api.post(`/devs/${id}/likes`, null, {
+      headers: { user: match.params.id }
+    });
+
+    setUsers(users.filter(user => user._id !== id));
+  }
+  
+  async function handleDislike(id) {
+    await api.post(`/devs/${id}/dislikes`, null, {
+      headers: { user: match.params.id }
+    });
+
+    setUsers(users.filter(user => user._id !== id));
+  } 
 
   return(
     <div className="main-container">
